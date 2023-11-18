77fcfd38990eec91f06241d53c6e2ab3fe5b2644
Satish Adhikari <126259184+satishadh-10@users.noreply.github.com>
2023-11-18 06:51:48 +0000

    Good One

---
+++ 06_iterations/seven.js
@@ -0,0 +1,11 @@
+// Reduce Method In Javascript
+
+
+const myNums = [1,2,3]
+
+const myTotal = myNums.reduce( function(acc, currval) {
+    console.log(`acc : ${acc} and currval : ${currval}`);
+    return acc + currval
+}, 0)
+
+console.log(myTotal);
